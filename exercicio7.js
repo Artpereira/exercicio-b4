6b2b59a7a1f050d55a797277b38a38046dbeafe2
art <art_dev@outlook.com>
2022-03-11 15:47:09 -0300

    Mudança exercicio7 e adição exercicio8

--- exercicio7.js
+++ exercicio7.js
@@ -1,5 +1,5 @@
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for(let lista = 0; lista < groceryList.length; lista +=1) {
+for(let lista = 0; lista < groceryList.length; lista +=2) {
     console.log(groceryList[lista]);
 }
