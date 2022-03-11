336f621e849b234d313885f03b666dbe05a54ca0
art <art_dev@outlook.com>
2022-03-11 09:59:16 -0300

    novos dados exercicio5

--- exercicio5.js
+++ exercicio5.js
@@ -15,32 +15,15 @@
 // let smaller = Math.min(a, b, c);
 // console.log(smaller);
 
-
-
-
-// const a = 20;
-// const b = 68;
-// const c = 250;
-
-// switch (a, b) {
-//     case a > b:
+// function biggestNumber(a, b, c) {
+//     if (a > b) {
 //         console.log(a);
-
-//     case b > a:
+//     } else if (b > a) {
 //         console.log(b);
-
+//     } else {
+//         console.log(c)
+//     }
 // }
 
-
-function biggestNumber(a, b, c) {
-    if (a > b && a > c) {
-        console.log(a);
-    } else if (b > a && b > c) {
-        console.log(b);
-    } else {
-        console.log(c)
-    }
-}
-
-biggestNumber(10, 20, 30);
+// biggestNumber(10, 20, 30);
 
